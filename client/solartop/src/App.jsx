c542db816be6ab89d3f5624e4ae5a10ef02dd4ac
Aryan107rn <aryannakade123@gmail.com>
2026-04-22 17:06:39 +0530

    Initial commit

---
+++ client/solartop/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-green-900 text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">SolarTop 🌞</h1>
+    </div>
+  )
+}
