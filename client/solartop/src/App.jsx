5a1f5a777efc5fe43b31f97cc5835791fe8c2fb3
Aryan107rn <aryannakade123@gmail.com>
2026-04-22 17:40:32 +0530

    added about.jsx

--- client/solartop/src/App.jsx
+++ client/solartop/src/App.jsx
@@ -2,13 +2,14 @@ import Navbar from './components/Navbar'
 import Hero from './sections/Hero'
 import Services from './sections/Services'
 import Projects from './sections/Projects'
-// add <Projects /> after <Services />
+import About from './sections/About'
 
 export default function App() {
   return (
     <main className="bg-[#FEFCE8] text-[#1a2e1a] overflow-x-hidden">
       <Navbar />
       <Hero />
+      <About />
       <Services />
       <Projects />
     </main>
